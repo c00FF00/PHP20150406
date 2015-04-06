9e3858c37672408a1e55671ffe754e356b9474b8
Grigory E <grigory.e@gmail.com>
2015-04-06 23:10:37 +0300

    Старт. Создаем структуру.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+    <link href="css/rs.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="wrapper">
+
+	<header class="header">
+		<strong>Новостной сайт</strong>
+
+	</header><!-- .header-->
+
+	<main class="content">
+		<strong>Content:</strong>
+    </main><!-- .content -->
+
+</div><!-- .wrapper -->
+
+<footer class="footer">
+	<strong>Footer:</strong> </footer><!-- .footer -->
+
+</body>
+</html>
