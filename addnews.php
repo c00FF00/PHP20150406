04aac0e6256575873db8d976307c4f4cedf30851
Grigory E <grigory.e@gmail.com>
2015-04-06 23:59:02 +0300

    Старт. Создаем структуру.

--- addnews.php
+++ addnews.php
@@ -17,18 +17,26 @@
 
     <header class="header">
         <br><br>
+
         <div><h2><strong>Новостной сайт</strong></h2></div>
         <nav>
-            <a href="read.php">Читать новости</a>&nbsp;  &nbsp;
-            <a href="addnews.php">Добавить новость</a>
+            <a href="read.php">Читать новости</a>&nbsp; &nbsp;
+            <a href="admin.php">Добавить новость</a>
         </nav>
 
     </header>
     <!-- .header-->
 
     <main class="content">
-        <h3><strong>Новость от .</strong></h3>
-        <p>Какая то новость</p>
+        <div class="fform">
+            <form action="addnews.php" method="post">
+                <div><input type="text" size="160" placeholder="Тема новости"></div>
+                <div><textarea cols="100" rows="10" placeholder="Введите новость...."></textarea></div>
+                <input type="submit" value="Разместить">
+
+            </form>
+        </div>
+
 
     </main>
     <!-- .content -->
