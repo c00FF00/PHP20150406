45861731575369524ee1f29a190a3b6d359b74a6
Grigory E <grigory.e@gmail.com>
2015-04-07 22:53:16 +0300

    Рабочий вариант. Сырой. Не выдержана архетиктура MVC. Будем исправлять.

--- view/readnews.php
+++ view/readnews.php
@@ -7,18 +7,17 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
     <script src="/js/bootstrap.min.js"></script>
-<!--    <link rel="stylesheet" href="/css/style.css">-->
+    <!--    <link rel="stylesheet" href="/css/style.css">-->
     <title>Главная</title>
 </head>
 
-
 <body>
-
 <div class="wrapper">
-
     <header class="header">
         <div class="page-header">
-            <h1><strong>Новостной сайт</strong> <small>и не только</small></h1>
+            <h1><strong>Новостной сайт</strong>
+                <small>и не только</small>
+            </h1>
         </div>
         <div class="panel panel-default">
             <div class="panel-body">
@@ -37,35 +36,28 @@
 
     $onenews = dbReadOneNews($_GET['id'])[0]; ?>
 
-
-
     <div class="content">
-
         <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">
-                        <?php echo $onenews['author']; ?>
-                        <?php echo $onenews['subject']; ?></h3>
-            </div>
+            <!--            <div class="panel-heading">-->
+            <h3 class="panel-title">
+                <ol class="breadcrumb">
+                    <li><?php echo $onenews['author']; ?></li>
+                    <li><?php echo $onenews['subject']; ?></li>
+                    <li class="active">Data</li>
+                </ol>
+            </h3>
+            <!--            </div>-->
             <div class="panel-body">
                 <?php echo $onenews['bodynews']; ?>
             </div>
         </div>
-
-
     </div>
     <!-- .content -->
-
 </div>
 <!-- .wrapper -->
 
 <footer class="footer">
     <strong>Footer:</strong></footer>
 <!-- .footer -->
-
-
-
-
-
 </body>
 </html>
