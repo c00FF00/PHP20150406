9bb0045500124deb920f7f61be8e7ab999629606
Grigory <grigory.e@gmail.com>
2015-04-08 12:43:34 +0300

    В работе.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><a href="#">Главная</a></li>
+                    <li role="presentation" class="active"><a>Главная</a></li>
                     <li role="presentation"><a href="/addnews.php">Добавить новость</a></li>
                 </ul>
             </div>
