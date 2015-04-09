d56aba9e1b4a73b3426a71268bae25c8b1404cdf
Grigory <grigory.e@gmail.com>
2015-04-09 09:27:55 +0300

    Переделываем

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
-<?php session_start(); session_unset(); ?>
+<?php
+session_start();
+session_unset();
+?>
 
 <?php
+include __DIR__ . '/model/dbread.php';
 
-switch ($_GET['task']) {
-    case 1:
-//        Читаем одну новость
-        include __DIR__ . '/model/dbread.php';
-        $onenews = dbReadOneNews($_GET['id'])[0];
-        include __DIR__ . '/view/readnews.php';
-        break;
-    default:
-//        Главная страница
-        include __DIR__ . '/model/dbread.php';
-        $result = dbReadAllSubjects();
-        include __DIR__ . '/view/newssubject.php';
-        break;
-}
+$result = dbReadAllSubjects();
+
+include __DIR__ . '/view/newssubject.php'
 
 ?>
 
