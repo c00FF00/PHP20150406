9ba69ceb2bf40fa12ff4f61a4023ae3d1c345b4c
Grigory E <grigory.e@gmail.com>
2015-04-13 18:24:21 +0300

    Мелка правка. ПОдчистка от коммента

--- newsedit.php
+++ newsedit.php
@@ -7,11 +7,6 @@ $data = ['id', 'date', 'author', 'subject', 'bodynews'];
 
 $onenew = $db->dbSelectColumnFromTable('main', $data, "WHERE id = " . $_GET['id']);
 
-
-//$data = ['id', 'date', 'author', 'subject'];
-//
-//$result = $db->dbSelectColumnFromTable('main', $data, 'ORDER BY DATE DESC');
-
 $onenews = $onenew[0];
 
 $art = new Article();
