306b260241f290516a4b0c825561e24b62fbff30
Grigory E <grigory.e@gmail.com>
2015-04-22 22:21:47 +0300

    Причесали код

--- classes/Db.php
+++ classes/Db.php
@@ -12,63 +12,6 @@ class Db
         $this->dbh = new PDO($dsn, $cfg['user'], $cfg['passw']);
     }
 
-    public function dbInsertRecord($dbTable, $data)
-    {
-        $columns = [];
-        $values = [];
-        foreach ($data as $column => $value) {
-            $columns[] = " `" . $column . "`";
-            $values[] = " '" . $value . "'";
-        }
-        $dbColumn = implode(',', $columns);
-        $dbValues = implode(',', $values);
-        $query = "INSERT INTO `" . $dbTable . "` ( " . $dbColumn . " ) VALUES ( " . $dbValues . " )";
-        $result = $this->dbExec($query);
-        if (false !== $result) {
-            return mysql_insert_id();
-        } else {
-            return $result;
-        }
-    }
-
-    public function dbSelectAll($dbTable)
-    {
-        $query = "SELECT * FROM `" . $dbTable . "`";
-        return $this->dbSelect($query);
-    }
-
-    public function dbSelectByColumns($dbTable, $data, $extsql = null)
-    {
-        $columns = implode(", ", $data);
-        $query = "SELECT " . $columns . " FROM `" . $dbTable . "` " . $extsql;
-        return $this->dbSelect($query);
-    }
-
-
-    public function dbUpdateRecord($dbTable, $data, $id)
-    {
-        $setdata = [];
-        foreach ($data as $column => $value) {
-            $value0 = " '" . $value . "'";
-            $setdata[] = " " . $column . " =" . $value0;
-        }
-        $dataset = implode(',', $setdata);
-        $query = "UPDATE " . $this->dbname . "." . $dbTable . " SET " . $dataset . " WHERE " . $dbTable . ".id = '" . $id . "'";
-        return $this->dbExec($query);
-
-    }
-
-    public function dbDeleteById($dbTable, $id)
-    {
-        $del = "DELETE FROM " . $this->dbname . "." . $dbTable . " WHERE " . $dbTable . ".id = " . $id;
-        return $this->dbExec($del);
-    }
-
-
-    //В новой концепции
-
-
-
     public function dbExecRet($sql, $params = [])
     {
         $sth = $this->dbh->prepare($sql, $params);
