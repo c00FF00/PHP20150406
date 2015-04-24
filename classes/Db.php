9781e89b1bf752fd79e78cbbc43735b40e69142c
Grigory <grigory.e@gmail.com>
2015-04-24 11:43:15 +0300

    Код. Удаление лишнего.

--- classes/Db.php
+++ classes/Db.php
@@ -25,12 +25,12 @@ class Db
         $sth->execute($params);
     }
 
-    public function dbSelect($sql)
-    {
-        $sth = $this->dbh->prepare($sql);
-        $sth->execute();
-        return $sth->fetchAll();
-    }
+//    public function dbSelect($sql)
+//    {
+//        $sth = $this->dbh->prepare($sql);
+//        $sth->execute();
+//        return $sth->fetchAll();
+//    }
 
     public function findAll($class, $sql, $params = [])
     {
