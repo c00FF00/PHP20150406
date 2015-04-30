02b92d64ca8301c338e81800fa6b0c5583fdd0c3
Grigory <grigory.e@gmail.com>
2015-04-30 13:45:34 +0300

    WORK IN PROGRESS...

--- index.php
+++ index.php
@@ -5,14 +5,6 @@ use App\Models;
 
 include __DIR__ . '/autoload.php';
 
-//if (Valid::Param($_GET['cntrl'], $_GET['act'])) {
-//    $controllerName = ucfirst($_GET['cntrl']) . 'Controller';
-//    $actionName = 'action' . $_GET['act'];
-//} else {
-//    $controllerName = 'NewsController';
-//    $actionName = 'actionAll';
-//}
-
 
 $ctrl = !empty($_GET['ctrl']) ? $_GET['ctrl'] : 'news';
 $act = !empty($_GET['act']) ? $_GET['act'] : 'all';
@@ -22,9 +14,3 @@ $controller = new $ctrlClassName;
 $method = 'action' . ucfirst($act);
 $controller->$method();
 
-//try {
-//    $controller = new $controllerName;
-//    $controller->$actionName();
-//} catch (E404Exception $e) {
-//    $controller->action404();
-//}
