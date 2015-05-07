ff5418f7423dc4f851f3b2552489dcd45b5ab0cd
Grigory E <grigory.e@gmail.com>
2015-05-07 22:26:34 +0300

    sss

--- model/Users.php
+++ model/Users.php
@@ -7,9 +7,20 @@ class Users extends Model {
 
     public $login;
     public $role;
+    public $passw;
 
-    public static function Auth()
+    public function Auth()
     {
+        $res = $this->findUser($this->login)[0];
+        var_dump($res);
+        foreach ($res as $r) {}
+        echo $res->role;
+        die;
+    }
+
+    public function Authv()
+    {
+
 
     }
 
