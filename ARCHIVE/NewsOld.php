fca4b4d9b7f5c8549445f607827afa17fb250582
Grigory <grigory.e@gmail.com>
2015-05-14 09:34:41 +0300

    Чистка проекта.

--- ARCHIVE/NewsOld.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include __DIR__ . '/../classes/BaseArticle.php';
-include __DIR__ . '/../classes/Article.php';
-include __DIR__ . '/../classes/Db.php';
-
-$db = new Db();
-
--- config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'host' => 'localhost',
-    'user' => 'root',
-    'passw' => '123456',
-    'dbname' => 'news'];
